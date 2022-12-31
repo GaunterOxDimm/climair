d33aa3cea29dbd036588b7e2560da2a185303107
Khaldon <khaldon@Khaldons-MacBook-Pro.local>
2022-12-31 18:25:57 +0100

    menu

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ImagesDiversRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/clim/contact', name: 'app_contact')]
+    public function index(ImagesDiversRepository $contact): Response
+    {
+        $contact_images = $contact->find(['id' => 5]);
+        return $this->render('contact/index.html.twig', [
+            'contact_images' => $contact_images,
+        ]);
+    }
+}
