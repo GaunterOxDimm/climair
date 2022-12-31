7af9c876439c6f0a49f5d820ccbb258c16f863ed
Khaldon <khaldon@Khaldons-MacBook-Pro.local>
2022-12-31 08:59:19 +0100

    menu + à propos + père noel

---
+++ src/Controller/AProposController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ImagesDiversRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Controller\AccueilController;
+
+class AProposController extends AbstractController
+{
+    #[Route('/clim/a-propos', name: 'app_a_propos', methods: ['GET'])]
+    public function index(ImagesDiversRepository $flambard): Response
+    {
+        $a_propos = $flambard->findOneBy(['id' => 6]);
+        return $this->render('a_propos/index.html.twig', [
+            'a-propos' => $a_propos,
+        ]);
+    }
+}
